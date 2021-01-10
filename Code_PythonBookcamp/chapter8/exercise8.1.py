a560fb9aa9a4d01f17f30c38f1ebe7afea147fbf
Vaskaran <41991945+Vaskaran@users.noreply.github.com>
2021-01-10 18:05:04 +0530

    Delete exercise8.1.py

--- Code_PythonBookcamp/chapter8/exercise8.1.py
+++
@@ -1,9 +0,0 @@
-# Exercise 8.1
-try:
-    result = 15/0
-except ArithmeticError:
-    print("Caught the ArithmeticError.Your divisor is zero.")
-except ZeroDivisionError:
-    print("Caught ZeroDivisionError.Correct the divisor which is zero at present.")
-
-
