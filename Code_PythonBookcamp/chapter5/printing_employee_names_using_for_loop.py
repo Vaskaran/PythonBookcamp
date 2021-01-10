83e10a66acd21d63f9b8e830a6684302383df3b2
Vaskaran <41991945+Vaskaran@users.noreply.github.com>
2021-01-10 18:03:46 +0530

    Delete printing_employee_names_using_for_loop.py

--- Code_PythonBookcamp/chapter5/printing_employee_names_using_for_loop.py
+++
@@ -1,8 +0,0 @@
-# Printing employee names using for loop
-employee_names = ["Sam", "Bob", "George", "Kate", "Julie"]
-print("Printing employee names using a 'for' loop:")
-for employee in employee_names:
-    print(employee)
-
-#for i in employee_names:
-#    print(i)
