25c0ca573afafa8e7203b607c686746042d9dc2b
Vaskaran <41991945+Vaskaran@users.noreply.github.com>
2021-01-10 18:08:51 +0530

    Delete using_function_alias.py

--- Code_PythonBookcamp/chapter7/using_function_alias.py
+++
@@ -1,6 +0,0 @@
-from my_library import make_total_double as mtd
-
-print("\nUsing the make_total_double function now.")
-print("Here mtd is an alias for make_total_double(...)")
-result = mtd(20, 30.5)
-print(result)
