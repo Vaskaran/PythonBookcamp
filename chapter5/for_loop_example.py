1b65b28e435260e57c9fea79f36a67da7ffd359e
Vaskaran <41991945+Vaskaran@users.noreply.github.com>
2021-01-16 12:18:18 +0530

    Delete for_loop_example.py

--- chapter5/for_loop_example.py
+++
@@ -1,8 +0,0 @@
-# Printing 1 to 10 using for loop
-print("Printing 1 to 10 using a 'for' loop:")
-# The function range(1,11) is used to print
-# values 1 to 11-1=10
-for current_value in range(1, 11):
-    print(current_value)
-# This statement is placed outside the for loop
-print("Job done.I've exited from the 'for' loop.")
