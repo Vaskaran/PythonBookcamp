20494c88d57a980fe4eff1dde3016048dcd00dd7
Vaskaran <41991945+Vaskaran@users.noreply.github.com>
2021-01-16 12:18:40 +0530

    Delete nested_loop_ex_1.py

--- chapter5/nested_loop_ex_1.py
+++
@@ -1,7 +0,0 @@
-print("*** Nested loop example.***")
-colors = ["Red", "Green", "Yellow"]
-fruits = ["Mango", "Banana"]
-for color in colors:
-    for fruit in fruits:
-        print(color, fruit, end=" ")
-    print()
